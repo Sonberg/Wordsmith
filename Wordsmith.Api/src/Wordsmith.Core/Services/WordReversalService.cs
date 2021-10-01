0c36ac4f6e3ade1ff4b929a624daf2375999977e
Per <per.sonberg@gmail.com>
2021-10-01 16:58:37 +0200

    implement Word reversal service

---
+++ Wordsmith.Api/src/Wordsmith.Core/Services/WordReversalService.cs
@@ -0,0 +1,29 @@
+﻿
+namespace Wordsmith.Core.Services
+{
+    using System.Linq;
+
+    public interface IWordReversalService {
+        string Transform(string value);
+    }
+
+    public class WordReversalService
+    {
+        public WordReversalService()
+        {
+        }
+
+        public string Transform(string value) {
+            return string.Join(" ", value.Split(" ").Select(Reverse));
+        }
+
+        private string Reverse(string word) {
+            var chars = word.ToCharArray();
+            var reversed = chars.Reverse().ToArray();
+
+            return new string(reversed);
+        }
+
+    }
+}
+
