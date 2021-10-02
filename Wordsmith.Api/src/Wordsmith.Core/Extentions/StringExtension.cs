6c67d0c555f47269d5cf8dca58fc287b39d78d13
Per <per.sonberg@gmail.com>
2021-10-02 16:57:19 +0200

    Implement reverse logic

---
+++ Wordsmith.Api/src/Wordsmith.Core/Extentions/StringExtension.cs
@@ -0,0 +1,25 @@
+﻿namespace Wordsmith.Core.Extentions
+{
+    public static class StringExtension
+    {
+        public static string ReverseWords(this string value)
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                return value;
+            }
+
+            return string.Join(" ", value.Split(" ").Select(ReverseWord));
+        }
+
+        private static string ReverseWord(string word)
+        {
+            var chars = word.ToCharArray();
+            var reversed = chars.Reverse().ToArray();
+
+            return new string(reversed);
+        }
+
+    }
+}
+
