858aa7052dd458080b70945267ab353778360997
Per <per.sonberg@gmail.com>
2021-10-03 07:28:02 +0200

    add SentenceReverser to page

--- Wordsmith.Web/pages/index.tsx
+++ Wordsmith.Web/pages/index.tsx
@@ -1,8 +1,11 @@
+import { SentenceReverser } from "components/SentenceReverser";
+
 export default function Page() {
   return (
     <div className="bg-gray-100 min-h-screen">
       <div className="container mx-auto px-4">
         <h1 className="text-4xl font-bold">Wordsmith</h1>
+        <SentenceReverser/>
       </div>
     </div>
   );
