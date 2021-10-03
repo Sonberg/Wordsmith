986d543b97e12bf3a6934193da802ee46dc2e170
Per <per.sonberg@gmail.com>
2021-10-03 08:21:48 +0200

    add Navigation component

---
+++ Wordsmith.Web/components/Navigation/Navigation.tsx
@@ -0,0 +1,7 @@
+export function Navigation() {
+  return (
+    <nav className="container mx-auto py-2">
+      <div className="font-bold uppercase">Wordsmith</div>
+    </nav>
+  );
+}
--- Wordsmith.Web/pages/index.tsx
+++ Wordsmith.Web/pages/index.tsx
@@ -1,11 +1,13 @@
 import { SentenceReverser } from "components/SentenceReverser";
+import { Navigation } from "components/Navigation";
 
 export default function Page() {
   return (
     <div className="bg-gray-100 min-h-screen">
-      <div className="container mx-auto px-4">
-        <h1 className="text-4xl font-bold">Wordsmith</h1>
-        <SentenceReverser/>
+      <Navigation />
+      <div className="container mx-auto px-4 min-h-screen mt-48">
+        <h3 className="text-xl mb-4">Try it, today!</h3>
+        <SentenceReverser />
       </div>
     </div>
   );
