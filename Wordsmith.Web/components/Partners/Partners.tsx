fa9ac9b5c4d91c64bb70492355861840dc4fbf39
Per <per.sonberg@gmail.com>
2021-10-05 15:33:36 +0200

    breakout Partners

--- Wordsmith.Web/components/Partners/Partners.tsx
+++ Wordsmith.Web/components/Partners/Partners.tsx
@@ -1,11 +1,33 @@
+/* eslint-disable @next/next/no-img-element */
+
 export function Partners() {
   return (
     <div className="flex flex-wrap justify-center pb-48">
-      <img src="/images/reddit.png" className="h-8 md:h-12 mx-4 my-4" />
-      <img src="/images/algolia.png" className="h-8 md:h-12 mx-4 my-4" />
-      <img src="/images/aops.png" className="h-8 md:h-12 mx-4 my-4" />
-      <img src="/images/casetext.png" className="h-8 md:h-12 mx-4 my-4" />
-      <img src="/images/koko.png" className="h-8 md:h-12 mx-4 my-4" />
+      <img
+        src="/images/reddit.png"
+        className="h-8 md:h-12 mx-4 my-4"
+        alt="Reddit logo"
+      />
+      <img
+        src="/images/algolia.png"
+        className="h-8 md:h-12 mx-4 my-4"
+        alt="Algolia logo"
+      />
+      <img
+        src="/images/aops.png"
+        className="h-8 md:h-12 mx-4 my-4"
+        alt="Art of problem solving logo"
+      />
+      <img
+        src="/images/casetext.png"
+        className="h-8 md:h-12 mx-4 my-4"
+        alt="Casetext logo"
+      />
+      <img
+        src="/images/koko.png"
+        className="h-8 md:h-12 mx-4 my-4"
+        alt="Koko logo"
+      />
     </div>
   );
 }
