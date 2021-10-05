088d6ab27f4d24aaa40a53c35f82814a7ac751ff
Per <per.sonberg@gmail.com>
2021-10-05 17:56:31 +0200

    remove disabled state

--- Wordsmith.Web/components/SentenceReverser/SentenceReverser.tsx
+++ Wordsmith.Web/components/SentenceReverser/SentenceReverser.tsx
@@ -15,7 +15,7 @@ export function SentenceReverser() {
   const [result, setResult] = useState("");
 
   const ref = useRef<HTMLInputElement | null>(null);
-  const debouncedValue = useDebounce(value, 300);
+  const debouncedValue = useDebounce(value, 200);
 
   const handleChange = useCallback(
     (event: ChangeEvent<HTMLInputElement>) => setValue(event.target.value),
@@ -66,7 +66,6 @@ export function SentenceReverser() {
         <input
           ref={ref}
           value={value}
-          disabled={isLoading}
           onChange={handleChange}
           placeholder="Write anything, we will reverse it"
           className="input font-semibold border-2 border-gray-400 appearance-none w-full px-3 py-3 px-2 focus focus:border-indigo-600 focus:outline-none active:outline-none active:border-indigo-600 max-w-4xl mx-auto"
