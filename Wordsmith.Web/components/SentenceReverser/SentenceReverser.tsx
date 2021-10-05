804127d274172d130b020220835719e661a3a1ee
Per <per.sonberg@gmail.com>
2021-10-05 16:34:03 +0200

    Trying to tackle React bug - input loses focus when Promise is called

--- Wordsmith.Web/components/SentenceReverser/SentenceReverser.tsx
+++ Wordsmith.Web/components/SentenceReverser/SentenceReverser.tsx
@@ -1,31 +1,57 @@
-import { ChangeEvent, useCallback, useState } from "react";
+import { ChangeEvent, useCallback, useEffect, useRef, useState } from "react";
 
 import { reverseSentence } from "services/api";
+import { useDebounce } from "hooks/useDebounce";
+
 import { Result } from "./components/Result";
 
 export function SentenceReverser() {
+  const [cache, setCache] = useState<Record<string, string>>({});
   const [value, setValue] = useState("");
   const [isLoading, setLoading] = useState(false);
   const [result, setResult] = useState("");
+
+  const ref = useRef<HTMLInputElement | null>(null);
+  const debouncedValue = useDebounce(value, 300);
 
   const handleChange = useCallback(
     (event: ChangeEvent<HTMLInputElement>) => setValue(event.target.value),
     []
   );
 
-  const handleSubmit = useCallback(async () => {
-    setLoading(true);
+  const handleSearch = useCallback(
+    async (value: string) => {
+      if (cache[value]) {
+        return setResult(cache[value]);
+      }
 
-    const { reversed } = await reverseSentence(value);
+      setLoading(true);
 
-    setResult(reversed);
-    setLoading(false);
-  }, [value]);
+      const { reversed } = await reverseSentence(value);
+
+      setResult(reversed);
+      setLoading(false);
+      setCache((state) => ({
+        ...state,
+        [value]: reversed,
+      }));
+
+      ref.current?.focus();
+    },
+    [cache]
+  );
+
+  useEffect(() => {
+    handleSearch(debouncedValue);
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [debouncedValue]);
 
   return (
     <>
       <div className="flex">
         <input
+          ref={ref}
           value={value}
           disabled={isLoading}
           onChange={handleChange}
